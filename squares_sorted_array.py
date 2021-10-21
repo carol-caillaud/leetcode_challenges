56751eff47ec6b85bafee2faef80b5f6bd4fd16b
carol.caillaud <carol.caillaud@gmail.com>
2021-10-21 15:57:25 -0300

    overtime_hour.py

---
+++ squares_sorted_array.py
@@ -0,0 +1,11 @@
+# retorne o quadrado de cada número, n ** 2, clasificado em ordem não decrescente
+def sortedSquares(nums): # função para ordenar o square array
+        n = 0
+        for n in range (nums):
+            n == n ** 2 #converter os elementos em seus valores em quadrado
+            n += 1
+        nums.sort (n) # retorna a lsta ordenada
+        
+        return n
+
+print (sortedSquares([-7,-3,2,3,11]))

0cca0956cb55e66068aedf522742ecf5accfc0dc
carol-caillaud <92691505+carol-caillaud@users.noreply.github.com>
2021-11-02 17:06:55 -0300

    Add files via upload

--- squares_sorted_array.py
+++ squares_sorted_array.py
@@ -1,11 +1,11 @@
 # retorne o quadrado de cada número, n ** 2, clasificado em ordem não decrescente
 def sortedSquares(nums): # função para ordenar o square array
-        n = 0
-        for n in range (nums):
-            n == n ** 2 #converter os elementos em seus valores em quadrado
-            n += 1
-        nums.sort (n) # retorna a lsta ordenada
+    i = 0
+    for n in nums:
+        nums[i] = n ** 2 #converter os elementos em seus valores em quadrado
+        n += 1
+    nums.sort() # retorna a lista ordenada e muda o array
         
-        return n
+    return nums
 
-print (sortedSquares([-7,-3,2,3,11]))
+print(sortedSquares([-7,-3,2,3,11]))
